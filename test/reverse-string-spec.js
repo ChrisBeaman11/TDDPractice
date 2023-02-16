f8d51ce286cf6d6e0e94210f43d3cd8747e8f321
Christopher Beaman <ChrisBeaman97@gmail.com>
2023-02-16 16:38:32 -0500

    reverse string

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,12 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const reverseString = require("../problems/reverse-string");
+
+describe("reverseString(string)", () => {
+    it("should return a reversed string", () => {
+        const str1 = "banana";
+        const actual1 = reverseString(str1);
+        expect(actual1).to.eql("ananab");
+
+    });
+});
