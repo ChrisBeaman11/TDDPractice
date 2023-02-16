d1f3c078e86fc1cf353dc7f66f8fe9bcb8d8fa3e
Christopher Beaman <ChrisBeaman97@gmail.com>
2023-02-16 18:06:44 -0500

    my-map

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,15 @@
 function myMap(inputArray, callback) {
- //test
+    let arr = [];
+    if(!inputArray.length){
+        return arr;
+    }
+    for(let i = 0; i<inputArray.length; i++){
+        arr.push(callback(inputArray[i], i, inputArray));
+    }
+    return arr;
 }
-
+// const callback1 = (num) => {
+//     return num + 1;
+// }
+// myMap([1, 2, 3], callback1);
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,13 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const myMap = require('../problems/my-map');
+describe("myMap(inputArray, callback", () =>{
+    it("should return a modified array with callback called on each element", () =>{
+        const arr1 = [1, 2, 3];
+        const callback1 = (num) => {
+            return num + 1;
+        }
+        const actual1 = myMap(arr1, callback1);
+        expect(actual1).to.eql([2, 3, 4]);
+    });
+});
